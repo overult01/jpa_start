ea8f2b02ab90ba6119cad719cbff42e9ba7950f3
it_st it <overult01@gmail.com>
2022-05-27 14:11:46 +0900

    delete

--- ex1/jpa_ex1/jpa_ex1-client/src/main/java/jpa_ex1/jpa_ex1/jpa_ex1/client/package-info.java
+++
@@ -1,5 +0,0 @@
-
-//TODO: Inserir aqui as definições de geração de código
-
-package jpa_ex1.jpa_ex1.jpa_ex1.client;
-
--- ex1/jpa_ex1/jpa_ex1-ws/src/main/java/jpa_ex1/jpa_ex1/jpa_ex1/ws/jaxrs/ServiceApplication.java
+++
@@ -1,8 +0,0 @@
-package jpa_ex1.jpa_ex1.jpa_ex1.ws.jaxrs;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class ServiceApplication extends Application {
-}
